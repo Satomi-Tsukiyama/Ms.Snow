6c292c40f54749dce5d0a45537eeeb08e801ceac
Satomi Taniguchi <sa103.ta2guchi@gmail.com>
2019-12-04 00:03:33 +0900

    commit from home

--- update.sql
+++ update.sql
@@ -1,1 +1,13 @@
 use sysnozawa;
+
+create table softdesign(
+	id integer primary key,
+    ankenid integer,
+    staffid integer,
+    manhours decimal,
+    insertdatetime timestamp,
+    updatedatetime timestamp);
+
+alter table softdesign add foreign key(ankenid) references anken(id);
+alter table softdesign add foreign key(staffid) references staff(id);
+alter table softdesign add constraint unique(id,ankenid);
